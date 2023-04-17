810d70a1a7d86855503c6aaa1d7b90d456de0fed
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-17 16:29:40 +0300

    Create ProductController.cs

---
+++ 01-Giriş/FirstDotnetCoreImplementations/FirstDotnetCoreImplementations/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDotnetCoreImplementations.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

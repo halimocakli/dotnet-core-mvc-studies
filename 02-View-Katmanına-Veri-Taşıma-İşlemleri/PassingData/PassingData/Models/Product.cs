5663e3b77f3a70355e06a6e97dce974ae564fb17
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-17 17:59:04 +0300

    Create Product.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingData/PassingData/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingData.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

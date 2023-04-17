06fb1d131a8b5ced53929c862ba6e807f228f161
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-17 16:00:03 +0300

    Create User.cs

---
+++ FirstNetCore/FirstNetCore/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstNetCore.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Surname { get; set; }
+    }
+}

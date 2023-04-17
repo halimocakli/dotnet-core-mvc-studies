397db9b3a6f353732e7d75f33a8fceffb88be387
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-17 17:59:01 +0300

    Create ProductController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingData/PassingData/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PassingData.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            string user_name = "Halim Can";
+            return View(user_name as Object);
+        }
+
+        public IActionResult ShowProduct()
+        {
+            Product product = new Product();
+            product.ProductName = "Television";
+            product.UnitPrice = 20;
+
+            return View(product);
+        }
+
+        public IActionResult GetProducList()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product()
+                {
+                    ProductName = "Computer",
+                    UnitPrice = 9600,
+                    CategoryName = "Electronics"
+                },
+
+                new Product()
+                {
+                    ProductName = "Mobil Phone",
+                    UnitPrice = 3200,
+                    CategoryName = "Electronics"
+                },
+
+                new Product()
+                {
+                    ProductName = "Refrigetor",
+                    UnitPrice = 5000,
+                    CategoryName = "White Appliances"
+                },
+
+                new Product()
+                {
+                    ProductName = "Chair",
+                    UnitPrice = 500,
+                    CategoryName = "Furniture"
+                }
+            };
+
+            return View(products);
+        }
+    }
+}

313b88feb1d84cb6c8e6f59942c3485777569813
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-17 16:00:00 +0300

    Create UserController.cs

---
+++ FirstNetCore/FirstNetCore/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstNetCore.Models;
+
+namespace FirstNetCore.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult GetUser()
+        {
+            User user = new User(); // Model katmanındaki User sınıfına ulaştık.
+            user.Name = "Halim Can";
+            user.Surname = "Ocaklı";
+            user.Phone = "+905417694139";
+            user.ID = 1;
+
+            return View();
+        }
+
+        /// <summary>
+        /// Aşağıdaki metot, UserController içerisinde oluşturulsa da bir Action değildir.
+        /// Bu nedenle [NonAction] attribute'u ile etiketlenir ve route yapısına dahil olmaz.
+        /// Ancak bir Controller içersinde Action dışında bir yapı kullanmak mimari açısından yanlıştır.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [NonAction]
+        public string GetFullName(User user)
+        {
+            return user.Name + " " + user.Surname;
+        }
+    }
+}

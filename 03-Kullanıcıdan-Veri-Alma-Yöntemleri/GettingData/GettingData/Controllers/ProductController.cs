96ad5ced1913e6dc622870293021db3be56fb638
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-30 14:25:01 +0300

    Create ProductController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GettingData/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace GettingData.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product(){ID=1, ProductName="Televizyon", CategoryName="Elektronik"},
+            new Product(){ID=2, ProductName="Araba", CategoryName="Otomobil"},
+            new Product(){ID=3, ProductName="Kalem", CategoryName="Kırtasiye"},
+            new Product(){ID=4, ProductName="Saat", CategoryName="Aksesuar"}
+        };
+        public IActionResult GetAllProducts()
+        {
+            return View(products);
+        }
+        public IActionResult GetProductById(int id)
+        {
+            Product product = products.Find(x => x.ID == id);
+
+            if (product == null)
+            {
+                ViewBag.Error = "Girdiğiniz ID ile eşleşen herhangi bir ürün bulunamadı.";
+            }
+
+            return View(product);
+        }
+    }
+}

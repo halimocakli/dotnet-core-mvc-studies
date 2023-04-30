8892a8ebae8b27082041f9dcf71f7e76ecf90af0
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-30 14:24:46 +0300

    Create ProductController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GetDataWithCategoryName/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using GetDataWithCategoryName.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GetDataWithCategoryName.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product(){ID=1, ProductName="Televizyon", CategoryName="Elektronik"},
+            new Product(){ID=2, ProductName="Araba", CategoryName="Otomobil"},
+            new Product(){ID=3, ProductName="Kalem", CategoryName="Kırtasiye"},
+            new Product(){ID=4, ProductName="Saat", CategoryName="Aksesuar"}
+        };
+        public IActionResult GetProductWithCategoryName(string categoryName)
+        {
+            List<Product> productList = products.FindAll(x => x.CategoryName == categoryName);
+
+            if (productList == null)
+            {
+                ViewBag.Error = "Girdiğiniz ID ile eşleşen herhangi bir ürün bulunamadı.";
+            }
+
+            return View(productList);
+        }
+    }
+}

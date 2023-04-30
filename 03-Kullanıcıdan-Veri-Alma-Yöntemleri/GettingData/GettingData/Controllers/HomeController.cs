575bfdf3b726bba53d8f23eabc225e1ac64c9603
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-30 14:24:59 +0300

    Update HomeController.cs

--- 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GettingData/Controllers/HomeController.cs
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GettingData/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GettingData.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GettingData.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index(string id, string productname)
         {
-            return View();
+            return "Application is running. Getting data from route.";
+        }
+
+        public string GetDataWithClass(Product product)
+        {
+            return "Application is running. Getting data from route with class";
         }
     }
 }

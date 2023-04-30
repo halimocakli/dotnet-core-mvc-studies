078896cc3c8b955577fb72e0f3da81fd1d108e88
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-04-30 14:24:45 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GetDataWithCategoryName/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetDataWithCategoryName.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Application is running. Getting data by category name.";
+        }
+    }
+}

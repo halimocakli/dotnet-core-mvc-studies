2552352930e6d9c38211b018b18959895210ae97
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-01 09:50:25 +0300

    Create HomeController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingTempDataAndTempDataTest/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingDataUsingTempDataAndTempDataTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Uygulamayı çalıştırdığımızda ilk olarak aşağıdaki metot tetiklenir.
+        // Startup dosyası üzerinde MapDefaultControllerRoute() metodu sebebiyle ilk olarak aşağıdaki metot çalışır.
+        public string Index()
+        {
+            return "PassingDataUsingTempDataAndTempDataTest ->> Controllers ->> HomeController ->> Index() Metodu ->> UYGULAMA ÇALIŞIYOR";
+        }
+    }
+}

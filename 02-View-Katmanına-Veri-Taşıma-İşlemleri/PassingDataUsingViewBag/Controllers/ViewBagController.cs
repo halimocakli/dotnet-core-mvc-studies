5ab6b0484c326fbd4e2bd0c3fdff2c503512167f
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-01 09:51:01 +0300

    Create ViewBagController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingViewBag/Controllers/ViewBagController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingDataUsingViewBag.Models;
+using System.Collections.Generic;
+
+namespace PassingDataUsingViewBag.Controllers
+{
+    public class ViewBagController : Controller
+    {
+        List<User> users = new List<User>()
+            {
+                new User()
+                {
+                    FirstName = "Halim Can",
+                    LastName = "Ocaklı",
+                    UserName = "halimaga"
+                },
+                new User()
+                {
+                    FirstName = "Raziye",
+                    LastName = "Dogan",
+                    UserName = "ıyaş"
+                },
+                new User()
+                {
+                    FirstName = "Oğuzhan",
+                    LastName = "Deniz",
+                    UserName = "sarıbaba"
+                },
+                new User()
+                {
+                    FirstName = "Batın Berkin",
+                    LastName = "Topaloğlu",
+                    UserName = "partalcı"
+                }
+            };
+        public IActionResult PassingDataViewBag()
+        {
+            ViewBag.PassedUsers = users;
+            return View();
+        }
+    }
+}

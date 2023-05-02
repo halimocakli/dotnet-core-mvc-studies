3faa6786c2c7e93269f706b241b2dcdfac9aaf70
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-02 12:00:20 +0300

    Delete User.cs

--- 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingData/PassingData/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PassingData.Models
-{
-    public class User
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string UserName { get; set; }
-    }
-}

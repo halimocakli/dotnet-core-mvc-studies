b13d3f9d5a9737d9d2893044f7d7760776dda06c
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-02 12:00:17 +0300

    Delete Product.cs

--- 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingData/PassingData/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PassingData.Models
-{
-    public class Product
-    {
-        public string ProductName { get; set; }
-        public decimal UnitPrice { get; set; }
-        public string CategoryName { get; set; }
-    }
-}

06e507436106aebf12ce2882dec620c1ffb9c881
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-03 00:13:44 +0300

    Create CustomerAndProductVM.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingViewModel/Models/ViewModel/CustomerAndProductVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PassingDataUsingViewModel.Models.ViewModel
+{
+    public class CustomerAndProductVM
+    {
+        public List<Customer> customers { get; set; }
+        public Product product { get; set; }
+        public string date { get; set; }
+    }
+}

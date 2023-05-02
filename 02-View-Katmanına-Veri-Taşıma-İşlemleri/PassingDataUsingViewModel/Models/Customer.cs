e483088bc205a93045a8e023c99952de4a1dab85
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-03 00:13:40 +0300

    Create Customer.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingViewModel/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingDataUsingViewModel.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}

9179c1994b3aa214ae8c0ddf6dceee23ae16ac34
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-03 00:13:38 +0300

    Create HomeController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingViewModel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingDataUsingViewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "PassingDataUsingViewModel ->> Controllers ->> HomeController ->> UYGULAMA ÇALIŞIYOR";
+        }
+    }
+}

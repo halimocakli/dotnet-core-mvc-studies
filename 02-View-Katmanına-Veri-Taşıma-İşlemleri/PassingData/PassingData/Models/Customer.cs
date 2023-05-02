dbade7b674022e48104f002c00d13cfad35d9317
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-02 12:00:15 +0300

    Delete Customer.cs

--- 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingData/PassingData/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PassingData.Models
-{
-    public class Customer
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string City { get; set; }
-    }
-}

3c7d2899396897a6cc8fc68a3ff878bade484a24
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-02 14:53:04 +0300

    Create User.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingDataUsingViewBag/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingDataUsingViewBag.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+}

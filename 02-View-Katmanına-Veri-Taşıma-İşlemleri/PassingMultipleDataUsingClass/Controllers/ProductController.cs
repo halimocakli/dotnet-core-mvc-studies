edbb1952dae38f2157c9a93627ddd3a11cd439d0
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:15 +0300

    Create ProductController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingClass/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingMultipleDataUsingClass.Models;
+
+namespace PassingMultipleDataUsingClass.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Models klasöründe oluşturduğumuz Product sınıfını örnekleyerek elde ettiğimiz veriyi View katmanına aktaralım.
+        public IActionResult ShowProduct()
+        {
+            Product product = new Product();
+
+            product.ProductName = "Television";
+            product.UnitPrice = 20;
+            product.CategoryName = "Electronics";
+
+            // View katmanına gönderdiğimiz veri Object tipinde.
+            return View(product);
+        }
+    }
+}

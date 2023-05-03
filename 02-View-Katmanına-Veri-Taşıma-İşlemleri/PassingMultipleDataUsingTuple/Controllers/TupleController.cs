0e1ff4fbaf78a765d864ebcf487bc7fd5141592e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:59 +0300

    Create TupleController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingTuple/Controllers/TupleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingMultipleDataUsingTuple.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PassingMultipleDataUsingTuple.Controllers
+{
+    public class TupleController : Controller
+    {
+        List<Customer> customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                FirstName = "Halim Can",
+                LastName = "Ocaklı",
+                City = "İstanbul"
+            },
+            new Customer()
+            {
+                FirstName = "Raziye",
+                LastName = "Dogan",
+                City = "Ankara"
+            },
+            new Customer()
+            {
+                FirstName = "Oğuzhan",
+                LastName = "Deniz",
+                City = "Erzurum"
+            },
+            new Customer()
+            {
+                FirstName = "Batın Berkin",
+                LastName = "Topaloğlu",
+                City = "Şiran"
+            }
+        };
+        public IActionResult PassingDataUsingTuple()
+        {
+            Product product = new Product();
+            product.ProductName = "Telephone";
+            product.CategoryName = "Electronics";
+            product.UnitPrice = 1500;
+
+            string date = DateTime.Now.ToString();
+
+            (List<Customer>, Product, string) customerAndProduct = (customers, product, date);
+
+            return View(customerAndProduct);
+        }
+    }
+}

8e5cae1edbe6d0b44e44e708a1731a1623a1530d
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:35 +0300

    Create HomeController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingList/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingMultipleDataUsingList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Uygulamayı çalıştırdığımızda ilk olarak aşağıdaki metot tetiklenir.
+        // Startup dosyası üzerinde MapDefaultControllerRoute() metodu sebebiyle ilk olarak aşağıdaki metot çalışır.
+        public string Index()
+        {
+            return "PassingMultipleDataUsingList ->> Controllers ->> HomeController ->> Index() Metodu ->> UYGULAMA ÇALIŞIYOR";
+        }
+    }
+}

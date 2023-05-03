b84e5b468cd6661704bbf9daac1237858a22da18
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:05:02 +0300

    Create Product.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingTuple/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingMultipleDataUsingTuple.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

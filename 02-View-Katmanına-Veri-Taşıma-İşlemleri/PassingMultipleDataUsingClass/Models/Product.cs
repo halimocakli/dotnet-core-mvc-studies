dc8f9b78e700368ec5a4fa0e50e37ac1290d0605
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:18 +0300

    Create Product.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingClass/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingMultipleDataUsingClass.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

680ec58b19c6151115ccee674ac53810e671f56d
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:05:01 +0300

    Create Customer.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingTuple/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingMultipleDataUsingTuple.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}

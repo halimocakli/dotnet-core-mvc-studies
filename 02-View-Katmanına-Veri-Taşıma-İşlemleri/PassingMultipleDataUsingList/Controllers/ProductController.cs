9da284b8c5d0f2fd4b45c836be7ea1bb8f44ecc8
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:37 +0300

    Create ProductController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingList/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassingMultipleDataUsingList.Models;
+using System.Collections.Generic;
+
+namespace PassingMultipleDataUsingList.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+            {
+                new Product()
+                {
+                    ProductName = "Computer",
+                    UnitPrice = 9600,
+                    CategoryName = "Electronics"
+                },
+
+                new Product()
+                {
+                    ProductName = "Mobil Phone",
+                    UnitPrice = 3200,
+                    CategoryName = "Electronics"
+                },
+
+                new Product()
+                {
+                    ProductName = "Refrigetor",
+                    UnitPrice = 5000,
+                    CategoryName = "White Appliances"
+                },
+
+                new Product()
+                {
+                    ProductName = "Chair",
+                    UnitPrice = 500,
+                    CategoryName = "Furniture"
+                }
+            };
+
+        // Yukarıda, global olarak tanımlanan ve oluşturulan, Product tipinde veri tutan listeyi View katmanına gönderen metot.
+        public IActionResult GetProductList()
+        {
+            return View(products);
+        }
+    }
+}

86b314c4ef6450ec971459a6dc5d1a9399f71d87
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-04 00:04:57 +0300

    Create HomeController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingMultipleDataUsingTuple/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingMultipleDataUsingTuple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "PassingMultipleDataUsingTuple ->> Controllers ->> HomeController ->> UYGULAMA ÇALIŞIYOR";
+        }
+    }
+}

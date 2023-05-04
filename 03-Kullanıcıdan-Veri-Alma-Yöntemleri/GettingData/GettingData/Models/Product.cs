b5312d9e934c36b0501f65463d2c1368eaee2f63
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-05 00:14:32 +0300

    Delete Product.cs

--- 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingData/GettingData/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GettingData.Models
-{
-    public class Product
-    {
-        public int ID { get; set; }
-        public string ProductName { get; set; }
-        public string CategoryName { get; set; }
-    }
-}

c80e182c7d0d33ae58a9c76845670bc7b74910ed
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-05 00:14:10 +0300

    Create ProductController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingSingleData/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PassingSingleData.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            string user_name = "Halim Can";
+
+            // View() metodu içerisindeki oveload'lara bakın.
+            // İlki View ismini string olarak istiyor, diğeri ise Object türünde bir değişken alıyor.
+            // Biz burada View'e object türünde bir değişken göndermek için type casting gerçekleştiriyoruz.
+            return View(user_name as Object);
+        }
+    }
+}

a43c8ab15589c4a718aeba7fffed87a26ccc4ae4
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-05 00:14:07 +0300

    Create HomeController.cs

---
+++ 02-View-Katmanına-Veri-Taşıma-İşlemleri/PassingSingleData/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassingSingleData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Uygulamayı çalıştırdığımızda ilk olarak aşağıdaki metot tetiklenir.
+        // Startup dosyası üzerinde MapDefaultControllerRoute() metodu sebebiyle ilk olarak aşağıdaki metot çalışır.
+        public string Index()
+        {
+            return "PassingSingleData ->> Controllers ->> HomeController ->> Index() Metodu ->> UYGULAMA ÇALIŞIYOR";
+        }
+    }
+}

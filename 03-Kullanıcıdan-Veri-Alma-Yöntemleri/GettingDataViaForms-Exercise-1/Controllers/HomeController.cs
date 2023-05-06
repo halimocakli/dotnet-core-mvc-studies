5714d2640534aaec9e13a21ef18275f6ddbff73e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-07 00:26:24 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-Exercise-1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingDataViaForms_Exercise_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Application is running. Getting data via HTML Forms Exercise.";
+        }
+    }
+}

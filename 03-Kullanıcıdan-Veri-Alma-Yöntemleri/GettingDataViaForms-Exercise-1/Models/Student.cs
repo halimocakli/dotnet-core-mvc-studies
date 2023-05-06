4c78e65e47803c4a448703a7497273a303ff3c10
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-07 00:26:31 +0300

    Create Student.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-Exercise-1/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace GettingDataViaForms_Exercise_1.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentID { get; set; }
+    }
+}

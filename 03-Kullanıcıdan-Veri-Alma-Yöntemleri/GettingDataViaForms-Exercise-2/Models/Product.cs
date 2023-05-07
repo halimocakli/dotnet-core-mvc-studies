e1423b738344ddba2f37cd49ce6d0baa8ed35c3e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-08 00:37:57 +0300

    Create Product.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-Exercise-2/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace GettingDataViaForms_Exercise_2.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

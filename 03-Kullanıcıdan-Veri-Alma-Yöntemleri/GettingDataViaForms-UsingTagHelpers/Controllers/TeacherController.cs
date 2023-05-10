5e0bc11e30c53fbedc1592dd09e95e41db209272
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-10 09:32:49 +0300

    Create TeacherController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-UsingTagHelpers/Controllers/TeacherController.cs
@@ -0,0 +1,53 @@
+﻿using GettingDataViaForms_UsingTagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GettingDataViaForms_UsingTagHelpers.Controllers
+{
+
+
+    public class TeacherController : Controller
+    {
+        List<Branch> branches = new List<Branch>()
+        {
+            new Branch()
+            {
+                ID = 1,
+                BranchName = "Matematik"
+            },
+            new Branch()
+            {
+                ID = 2,
+                BranchName = "İngilizce"
+            },
+            new Branch()
+            {
+                ID = 3,
+                BranchName = "Biyoloji"
+            },
+            new Branch()
+            {
+                ID = 4,
+                BranchName = "Fizik"
+            },
+            new Branch()
+            {
+                ID = 5,
+                BranchName = "Kimya"
+            }
+        };
+
+        public IActionResult AddTeacher()
+        {
+            ViewBag.Branches = branches;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTeacher(Teacher teacher)
+        {
+            ViewBag.Branches = branches;
+            return View();
+        } 
+    }
+}

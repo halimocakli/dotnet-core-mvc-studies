2c0a778383069111c1d8998b2ddac20ac2b9fdc3
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-10 17:42:18 +0300

    Create Branch.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-UsingTagHelpers/Models/Branch.cs
@@ -0,0 +1,8 @@
+﻿namespace GettingDataViaForms_UsingTagHelpers.Models
+{
+    public class Branch
+    {
+        public int ID { get; set; }
+        public string BranchName { get; set; }
+    }
+}

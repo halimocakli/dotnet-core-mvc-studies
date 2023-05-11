2c36cc63a908c9fcf8d37a32f97e751fc5560eec
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-11 06:58:25 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms-UsingTupleObject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingDataViaForms_UsingTupleObject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Application is running. Getting data via HTML Forms using Tuple Object.";
+        }
+    }
+}

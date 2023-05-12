757328afd3729d6b1ebe504f3d95c4a1726bb67b
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-12 07:17:54 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaQueryString-Exercise/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingDataViaQueryString_Exercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Application is running. Getting data via Query string according to category name.";
+        }
+    }
+}

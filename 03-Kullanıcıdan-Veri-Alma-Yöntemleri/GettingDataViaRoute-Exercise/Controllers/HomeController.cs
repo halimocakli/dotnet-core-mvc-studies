0aafa5d0979726b1d3c9c4ae06735cd240e86dd4
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-12 09:29:55 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaRoute-Exercise/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingDataViaRoute_Exercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Application is running. Getting data by category name.";
+        }
+    }
+}

bc7de4a3ca0fb09b543ec927ed8e26b0a649bf37
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-12 07:17:25 +0300

    Create UserController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using GettingDataViaForms.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GettingDataViaForms.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            return View();
+        }
+    }
+}

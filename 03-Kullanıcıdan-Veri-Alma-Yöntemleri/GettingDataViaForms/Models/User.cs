ea1d3030d674a483db4cad754e1780c3bb738346
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-12 07:17:32 +0300

    Create User.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaForms/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GettingDataViaForms.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}

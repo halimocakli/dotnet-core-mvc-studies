341aae1064308b5a61ce2086c14a06cbf0fbb1f7
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-12 09:30:30 +0300

    Create HomeController.cs

---
+++ 03-Kullanıcıdan-Veri-Alma-Yöntemleri/GettingDataViaRoute/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GettingDataViaRoute.Models;
+
+namespace GettingDataViaRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string id, string productname)
+        {
+            return "Application is running. Getting data from route. Parameters: id(string), productname(string).";
+        }
+
+        public string GetDataWithClass(Product product)
+        {
+            return "Application is running. Getting data from route with class. Parameters: product(class)";
+        }
+    }
+}

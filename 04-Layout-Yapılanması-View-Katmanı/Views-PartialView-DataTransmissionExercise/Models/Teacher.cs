0435ad2c621d0c085e7e642ca9a6807d2a228927
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-17 15:40:49 +0300

    Create Teacher.cs

---
+++ 04-Layout-Yapılanması-View-Katmanı/Views-PartialView-DataTransmissionExercise/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace Views_PartialView_DataTransmissionExercise.Models
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+}

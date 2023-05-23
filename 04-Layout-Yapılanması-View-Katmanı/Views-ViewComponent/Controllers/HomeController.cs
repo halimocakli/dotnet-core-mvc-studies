958a3e127d5224e26b02b3728f174be3cb7b5d6b
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-23 08:52:55 +0300

    Create HomeController.cs

---
+++ 04-Layout-Yapılanması-View-Katmanı/Views-ViewComponent/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Views_ViewComponent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page_1() 
+        {
+            return View();
+        }
+        public IActionResult Page_2()
+        {
+            return View();
+        }
+        public IActionResult Page_3()
+        {
+            return View();
+        }
+        public IActionResult LoginAsTeacher()
+        {
+            return View();
+        }
+        public IActionResult LoginAsStudent()
+        {
+            return View();
+        }
+        public IActionResult LoginAsParent()
+        {
+            return View();
+        }
+    }
+}

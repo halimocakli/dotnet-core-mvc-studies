1829a86c8219d4aa5b96486a92ceb2f4040d156f
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-23 08:53:02 +0300

    Create WorkChart.cs

---
+++ 04-Layout-Yapılanması-View-Katmanı/Views-ViewComponent/Models/WorkChart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Views_ViewComponent.Models
+{
+    public class WorkChart
+    {
+        public WorkChart(string _teacherName, List<bool> _chart)
+        {
+            TeacherName = _teacherName;
+            Chart = _chart;
+        }
+
+        public string TeacherName { get; set; }
+        public List<bool> Chart { get; set; }
+    }
+}

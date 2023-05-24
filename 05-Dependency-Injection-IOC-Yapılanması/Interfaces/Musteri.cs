4490f811ddd2670d0e993e7496e4eb5ca32832ae
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-25 00:17:36 +0300

    Create Musteri.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/Interfaces/Musteri.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Musteri : IMusteri
+    {
+        private int id;
+        private string isim;
+        private string soyisim;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+
+        public string SoyIsim
+        {
+            get { return soyisim; }
+            set { soyisim = value; }
+        }
+
+        public int YeniKayit(string isim, string soyisim)
+        {
+            Console.WriteLine("YeniKayit() metodu çalıştı.");
+            return 1;
+        }
+
+        public int KayitDuzenle(int id, string isim, string soyisim)
+        {
+            Console.WriteLine("KayitDuzenle() metodu çalıştı.");
+            return 1;
+        }
+
+        public int KayitSil(int id)
+        {
+            Console.WriteLine("KayitSil() metodu çalıştı.");
+            return 1;
+        }
+    }
+}

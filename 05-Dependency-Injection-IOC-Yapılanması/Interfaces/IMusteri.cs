b505de9272d90e714fa3dda3547a1df8c78752a8
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-25 00:17:28 +0300

    Create IMusteri.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/Interfaces/IMusteri.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal interface IMusteri
+    {
+        // Field'lar
+        int Id { get; set; }
+        string Isim { get; set; }
+        string SoyIsim { get; set; }
+
+        // Metotlar
+        int YeniKayit(string isim, string soyisim);
+        int KayitDuzenle(int id, string isim, string soyisim);
+        int KayitSil(int id);
+    }
+}

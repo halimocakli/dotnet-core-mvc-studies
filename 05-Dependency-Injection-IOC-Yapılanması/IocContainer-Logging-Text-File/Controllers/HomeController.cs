6eb1dc5ad6d380f49e7d03889aa2cdd8c6cc8f51
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-26 15:59:44 +0300

    Create HomeController.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/IocContainer-Logging-Text-File/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using IocContainer_Logging_Text_File.Services;
+using IocContainer_Logging_Text_File.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IocContainer_Logging_Text_File.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Dependency Injection
+        // ConsoleLogger _log;
+        ILog _log;
+        public HomeController(ILog log)
+        {
+            _log = log;
+        }
+
+        public string Index()
+        {
+            //ConsoleLogger log = new ConsoleLogger();
+            //log.Info("Yeni bir istek gelmiştir");
+
+            _log.Info("Index Controller'ı içerisine yeni bir istek gelmiştir");
+            System.Threading.Thread.Sleep(2000);
+            _log.Info("Index Controller'ı içerisine yeni bir istek gelmiştir");
+            System.Threading.Thread.Sleep(2000);
+            _log.Info("Index Controller'ı içerisine yeni bir istek gelmiştir");
+
+            return "Service is running.";
+        }
+    }
+}

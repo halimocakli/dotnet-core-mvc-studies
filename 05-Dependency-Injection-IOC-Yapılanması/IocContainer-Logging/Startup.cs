603b938225ac0161b5619dd5667f798a1a0cc086
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-27 10:59:18 +0300

    Create Startup.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/IocContainer-Logging/Startup.cs
@@ -0,0 +1,46 @@
+using IocContainer_Logging.Services.Interfaces;
+using IocContainer_Logging.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IocContainer_Logging
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSingleton<ConsoleLogger>();
+            //services.AddSingleton<ILog, ConsoleLogger>();
+            services.AddSingleton<ILog, TextLogger>();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute("CustomRoute", "{controller=home}/{action=Index}/{id?}");
+                });
+            });
+        }
+    }
+}

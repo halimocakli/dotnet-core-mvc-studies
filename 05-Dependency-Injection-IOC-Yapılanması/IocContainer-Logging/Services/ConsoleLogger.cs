ca8d57329a0c5813743f491f428d01b18be77358
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-27 10:59:10 +0300

    Create ConsoleLogger.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/IocContainer-Logging/Services/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using IocContainer_Logging.Services.Interfaces;
+using System;
+
+namespace IocContainer_Logging.Services
+{
+    public class ConsoleLogger : ILog
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

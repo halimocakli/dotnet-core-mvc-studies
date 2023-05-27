a601e49c7fd19f83fdff2893932601053be752bd
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-27 10:59:15 +0300

    Create TextLogger.cs

---
+++ 05-Dependency-Injection-IOC-Yapılanması/IocContainer-Logging/Services/TextLogger.cs
@@ -0,0 +1,14 @@
+﻿using IocContainer_Logging.Services.Interfaces;
+using System;
+using System.Reflection.Metadata;
+
+namespace IocContainer_Logging.Services
+{
+    public class TextLogger : ILog
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine("Dosyaya yazılan mesaj: " + message);
+        }
+    }
+}

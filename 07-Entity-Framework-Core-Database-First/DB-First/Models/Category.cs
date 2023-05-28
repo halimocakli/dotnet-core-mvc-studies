2545ef9e9bea403cbf3e9547840038758fe8793d
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 00:49:24 +0300

    Create Category.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}

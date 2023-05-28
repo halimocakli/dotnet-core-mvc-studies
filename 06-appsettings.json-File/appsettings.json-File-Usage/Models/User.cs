d1c5178b33029f9937f1318360303e2446888fb2
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-28 09:31:25 +0300

    Create User.cs

---
+++ 06-appsettings.json-File/appsettings.json-File-Usage/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace appsettings.json_File_Usage.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

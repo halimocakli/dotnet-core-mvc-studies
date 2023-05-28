239d6961132b01db1e41c654f4d1674c09606c23
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 00:49:15 +0300

    Create CategoryController.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using DB_First.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DB_First.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NORTHWINDContext _db;
+        public CategoryController(NORTHWINDContext db)
+        {
+            _db = db;
+        }
+        public IActionResult CategoryList()
+        {
+            List<Category> categorylist = _db.Categories.ToList();
+            return View(categorylist);
+        }
+    }
+}

a7e0135234be88311ef225fc5689b9aeede49a6c
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 00:49:25 +0300

    Create CategorySalesFor1997.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/CategorySalesFor1997.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class CategorySalesFor1997
+    {
+        public string CategoryName { get; set; }
+        public decimal? CategorySales { get; set; }
+    }
+}

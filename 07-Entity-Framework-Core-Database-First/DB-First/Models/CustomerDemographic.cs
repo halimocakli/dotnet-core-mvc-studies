c2618c439ef8d2a5e61d9dca5c8bc5a1e560e293
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 09:06:10 +0300

    Create CustomerDemographic.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/CustomerDemographic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class CustomerDemographic
+    {
+        public CustomerDemographic()
+        {
+            CustomerCustomerDemos = new HashSet<CustomerCustomerDemo>();
+        }
+
+        public string CustomerTypeId { get; set; }
+        public string CustomerDesc { get; set; }
+
+        public virtual ICollection<CustomerCustomerDemo> CustomerCustomerDemos { get; set; }
+    }
+}

4bf49c01a3c6fa845dabc047b71954fb2ac4e108
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 09:06:08 +0300

    Create CustomerAndSuppliersByCity.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/CustomerAndSuppliersByCity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class CustomerAndSuppliersByCity
+    {
+        public string City { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Relationship { get; set; }
+    }
+}

fb72323bafe6e25295f00636099bf24aca18d263
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 09:06:16 +0300

    Create EmployeeTerritory.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/EmployeeTerritory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class EmployeeTerritory
+    {
+        public int EmployeeId { get; set; }
+        public string TerritoryId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Territory Territory { get; set; }
+    }
+}

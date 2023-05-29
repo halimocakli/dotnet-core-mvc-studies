a002b68624a754f4e8c661d889fad0abcc848859
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-29 09:06:12 +0300

    Create CustomerCustomerDemo.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/CustomerCustomerDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class CustomerCustomerDemo
+    {
+        public string CustomerId { get; set; }
+        public string CustomerTypeId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual CustomerDemographic CustomerType { get; set; }
+    }
+}

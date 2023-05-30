ccedb8422e70bdb88a2b6b3c9d7d57d92f7c05a4
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-30 07:05:40 +0300

    CodeFirst

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-Data-Annotations/Context/MyDbContext.cs
@@ -0,0 +1,49 @@
+﻿using CodeFirst_Data_Annotations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_Data_Annotations.Context
+{
+    public class MyDbContext : DbContext
+    {
+        // Dependency Injection yöntemi ile DB bağlantısı kurma yöntemi.
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        // OnConfiguring metodu ile DB bağlantısı kurulur.
+        // DB bağlantısı kurmak için kullanılan alternatiflerden biridir.
+        // Genellikle ctor metot üzerinen DB bağlantısı kurulur ki bu yöntem daha efektifdir.
+        // Aşağıdaki yöntemi de görmüş olmak için üzerinde duruyoruz.
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=BOOKSTORE;uid:sa;pwd=123");
+            base.OnConfiguring(optionsBuilder);
+        }
+        */
+
+        // Veri tabanımızın konfigürasyonlarının yapıldığı yer OnModelCreating() metodudur.
+        // Bu metor veri tabanı ilk defa oluşturulurken tetiklenir.
+        // Bu metot sayesinde, veri tabanı oluşmadan önce;
+        //  - Tabloların ismini değiştirebiliriz,
+        //  - Kolon özelliklerini, veri tiplerini belirleyebiliriz.
+        // FLUENT API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetail>().Ignore("ID");
+            modelBuilder.Entity<OrderDetail>().HasKey("OrderID", "ProductID");
+        }
+
+        // Veri tabanı ile kod üzerindeki sınıflarımız arasındaki ilişkiyi belirlemek için DbSet yazılmalıdır.
+        // Böylece yazdığımız tablo sınıflarının veri tabanı üzerinde bir karşılığı olur.
+        // Hazırladığımız her bir class için aşağıdaki işlemleri yapmamız gerekiyor.
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<AppUserDetail> UserDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-Data-Annotations/Enums/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirst_Data_Annotations.Enums
+{
+    public enum Role
+    {
+        Admin = 1,
+        User = 2,
+        Visitor = 3
+    }
+}

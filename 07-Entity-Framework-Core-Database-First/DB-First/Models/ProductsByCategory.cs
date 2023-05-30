c01989498ef7a64a3f792784447a4cb6d81e1dc7
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-30 07:04:46 +0300

    Create ProductsByCategory.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/ProductsByCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class ProductsByCategory
+    {
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public short? UnitsInStock { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}

c4332f58a87fe28844219a0dbb1535b20fc78421
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-05-30 07:04:38 +0300

    Create OrderSubtotal.cs

---
+++ 07-Entity-Framework-Core-Database-First/DB-First/Models/OrderSubtotal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DB_First.Models
+{
+    public partial class OrderSubtotal
+    {
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}

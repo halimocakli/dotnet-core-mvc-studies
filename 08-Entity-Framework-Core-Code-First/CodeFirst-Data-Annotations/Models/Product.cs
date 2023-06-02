da6435b105cfc7c954dd1645605bde4e3d8a3497
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-02 09:10:02 +0300

    Create Product.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-Data-Annotations/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirst_Data_Annotations.Models
+{
+    [Table("Ürünler")]      // Tablo database'de açılırken "Ürünler" adı ile açılacak
+    public class Product : BaseEntity
+    {
+        [Column("ÜrünAdı", TypeName = "nvarchar(50)")]     // Sütun tabloya eklenirken "ÜrünAdı" şeklinde eklenecek ve tipi nvarchar(50) olacak.
+        [MinLength(5)]
+        [Required]
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        [Column("BirimFiyat", TypeName = "numeric(18,1)")]
+        public decimal? UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+
+        // Foreign Key for Product - Category : ONE-TO-MANY relationship.
+        public int CategoryID { get; set; }
+
+        // Relational Property
+        public virtual Category Category { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+    }
+}

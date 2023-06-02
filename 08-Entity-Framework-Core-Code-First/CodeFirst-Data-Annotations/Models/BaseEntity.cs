bc1382f5f75ab877a5ecb84568576780014158c1
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-02 09:09:36 +0300

    Create BaseEntity.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-Data-Annotations/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirst_Data_Annotations.Enums;
+using System;
+
+namespace CodeFirst_Data_Annotations.Models
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            Status = DataStatus.Inserted;
+        }
+
+        public int ID { get; set; }
+        public DataStatus Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}

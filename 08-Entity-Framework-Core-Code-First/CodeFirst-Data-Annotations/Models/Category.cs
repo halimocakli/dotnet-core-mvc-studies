ff916e1c65333e2f4cab5534e6df1ab419605d6c
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-02 09:09:48 +0300

    Create Category.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-Data-Annotations/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirst_Data_Annotations.Models
+{
+    [Table("Kategoriler")]
+    public class Category : BaseEntity
+    {
+        [Required]
+        [MaxLength(30)]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "nvarchar(300)")]
+        public string CategoryDescription { get; set; }
+
+        // Relational Property
+        public virtual List<Product> Products { get; set; }
+    }
+}

41534d1ab7b623c595aed87306b4565dfa8e615b
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-03 10:10:57 +0300

    Create OrderDetail.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-DbContext-Class/Models/OrderDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeFirst_DbContext_Class.Models
+{
+    public class OrderDetail : BaseEntity
+    {
+        public short Quantity { get; set; }
+        public decimal? TotalPrice { get; set; }
+    }
+}

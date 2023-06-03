80f90045bdda0155edca540118ba33e643ccbe16
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-03 10:10:54 +0300

    Create Order.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-DbContext-Class/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFirst_DbContext_Class.Models
+{
+    public class Order : BaseEntity
+    {
+        // Foreign Key for Order - AppUser : ONE-TO-MANY relationship.
+        public int AppUserId { get; set; }
+
+        // Relational Property
+        public virtual AppUser AppUser { get; set; }
+    }
+}

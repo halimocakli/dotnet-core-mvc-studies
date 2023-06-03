47dd29bfbb8e18a85f459fb8a1c5826473852923
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-03 10:10:52 +0300

    Create Category.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-DbContext-Class/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CodeFirst_DbContext_Class.Models
+{
+    public class Category : BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        // Relational Property
+        public virtual List<Product> Products { get; set; }
+    }
+}

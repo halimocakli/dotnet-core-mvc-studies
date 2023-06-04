4533aa6e96769c2bc4f2945a47eccb7d96fdbee0
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-04 10:36:25 +0300

    Create AuthorConfiguration.cs

---
+++ 08-Entity-Framework-Core-Code-First/CodeFirst-FluentApi-LibraryManagementSystem/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirst_FluentApi_LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace CodeFirst_FluentApi_LibraryManagementSystem.Configurations
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.ToTable("Yazarlar");
+            builder.Property(x => x.FirstName).HasColumnName("Isim");
+            builder.Property(x => x.LastName).HasColumnName("Soyisim");
+        }
+    }
+}

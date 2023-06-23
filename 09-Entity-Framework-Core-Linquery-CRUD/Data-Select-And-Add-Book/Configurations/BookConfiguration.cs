1d890197cbd9ea9c27d9c6500df3fa8ba297c710
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-23 11:03:07 +0300

    EF Core - LINQ - CRUD

---
+++ 09-Entity-Framework-Core-Linquery-CRUD/Data-Select-And-Add-Book/Configurations/BookConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Data_Select_And_Add_Book.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data_Select_And_Add_Book.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasOne(b => b.Author).WithMany(a => a.Books).HasForeignKey(b => b.AuthorID);
+            builder.HasOne(b => b.BookType).WithMany(bt => bt.Books).HasForeignKey(b => b.BookTypeID);
+        }
+    }
+}
---
+++ 09-Entity-Framework-Core-Linquery-CRUD/Data-Select-And-Add-Book/Configurations/FineConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Data_Select_And_Add_Book.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data_Select_And_Add_Book.Configurations
+{
+    public class FineConfiguration : IEntityTypeConfiguration<Fine>
+    {
+        public void Configure(EntityTypeBuilder<Fine> builder)
+        {
+            builder.HasOne(f => f.Loan).WithMany(l => l.Fines).HasForeignKey(f => f.LoanID);
+            builder.HasOne(f => f.Member).WithMany(m => m.Fines).HasForeignKey(f => f.MemberID);
+
+            builder.Property(x => x.FineAmount).IsRequired().HasColumnType("numeric(18,1)");
+        }
+    }
+}

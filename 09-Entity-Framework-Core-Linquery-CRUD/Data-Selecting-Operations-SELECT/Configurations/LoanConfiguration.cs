1d78c2821638af1957e45716655fa507cc4aa420
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-26 00:09:40 +0300

    EF Core - LINQ  - CRUD

---
+++ 09-Entity-Framework-Core-Linquery-CRUD/Data-Selecting-Operations-SELECT/Configurations/LoanConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Data_Selecting_Operations_SELECT.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data_Selecting_Operations_SELECT.Configurations
+{
+    public class LoanConfiguration : IEntityTypeConfiguration<Loan>
+    {
+        public void Configure(EntityTypeBuilder<Loan> builder)
+        {
+            builder.HasOne(l => l.Book).WithMany(b => b.Loans).HasForeignKey(l => l.BookID);
+            builder.HasOne(l => l.Member).WithMany(m => m.Loans).HasForeignKey(l => l.MemberID);
+        }
+    }
+}

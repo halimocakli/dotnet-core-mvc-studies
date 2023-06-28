dc8b53f910b1eef1aabe5565a43d48dc59781146
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-06-29 00:40:55 +0300

    Generic Repository Design Pattern

---
+++ 10-Generic-Repository-Design-Patern/Concreate-Repository/Configurations/BookConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Concreate_Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Concreate_Repository.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasOne(b => b.Author).WithMany(a => a.Books).HasForeignKey(b => b.AuthorID);
+            builder.HasOne(b => b.BookType).WithMany(bt => bt.Books).HasForeignKey(b => b.BookTypeID);
+        }
+    }
+}

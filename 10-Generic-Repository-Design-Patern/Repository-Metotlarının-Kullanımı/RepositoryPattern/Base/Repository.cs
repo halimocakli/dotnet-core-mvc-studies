80119159fa611cf3ffcd07654f4ae9611d0cfd27
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-07-04 00:28:46 +0300

    Generic Repository Design Pattern

---
+++ 10-Generic-Repository-Design-Patern/Repository-Metotlarının-Kullanımı/RepositoryPattern/Base/Repository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository_Metotlarının_Kullanımı.Context;
+using Repository_Metotlarının_Kullanımı.Models;
+using Repository_Metotlarının_Kullanımı.RepositoryPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository_Metotlarının_Kullanımı.RepositoryPattern.Base
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        MyDbContext _db;
+        DbSet<T> table;
+        public Repository(MyDbContext db)
+        {
+            _db = db;
+            table = _db.Set<T>();
+        }
+
+        private void Save()
+        {
+            _db.SaveChanges();
+        }
+        public void Add(T entity)
+        {
+            table.Add(entity);
+            Save();
+        }
+
+        /// <summary>
+        /// Bir tabloda aradığımız kriterleri sağlayan bir veri olup olmadığını kontrol etmemizi sağlayan metot.
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns>bool type value</returns>
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            return table.Any(exp);
+        }
+
+        /// <summary>
+        /// Bir tablodaki satır sayısını elde etmemizi sağlayan metot.
+        /// </summary>
+        /// <returns>Integer type value</returns>
+        public int Count()
+        {
+            return table.Count();
+        }
+
+        /// <summary>
+        /// Soft Delete yapmamızı sağlayan metot
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+            T entity = GetById(id);
+            entity.Status = Enums.DataStatus.Deleted;
+            entity.UpdatedDateTime = DateTime.Now;
+
+            table.Update(entity);
+            Save();
+        }
+
+        /// <summary>
+        /// Halihazırda veri tabanı üzerine aktif, yani silinmemiş, durumda olan verileri elde etmemizi sağlayan metot.
+        /// </summary>
+        /// <returns>List<T></returns>
+        public List<T> GetActiveData()
+        {
+            return table
+                .Where(x => x.Status != Enums.DataStatus.Deleted)
+                .ToList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public List<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public List<T> GetByFilter(Expression<Func<T, bool>> exp)
+        {
+            return table.Where(exp).ToList();
+        }
+
+        /// <summary>
+        /// Tablo üzerinde, "id" değeri ile veri aramamızı sağlayan metot.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>T type value</returns>
+        public T GetById(int id)
+        {
+            return table.Find(id);
+        }
+
+        public List<T> SelectActivesByLimit(int limit)
+        {
+            return table
+                .Where(x => x.Status != Enums.DataStatus.Deleted)
+                .Take(limit)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Hard delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void SpecialDelete(int id)
+        {
+            T entity = GetById(id);
+            table.Remove(entity);
+            Save();
+        }
+
+        public void Update(T entity)
+        {
+            entity.Status = Enums.DataStatus.Updated;
+            entity.UpdatedDateTime = DateTime.Now;
+            table.Update(entity);
+            Save();
+        }
+    }
+}
---
+++ 10-Generic-Repository-Design-Patern/Repository-Metotlarının-Kullanımı/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Repository_Metotlarının_Kullanımı.Context;
+using Repository_Metotlarının_Kullanımı.Models;
+using Repository_Metotlarının_Kullanımı.RepositoryPattern.Base;
+using Repository_Metotlarının_Kullanımı.RepositoryPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository_Metotlarının_Kullanımı
+{
+    public class Startup
+    {
+        readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyDbContext>(options => options.UseSqlServer(_configuration["ConnectionStrings:MsSql"]));
+            services.AddControllersWithViews();
+            services.AddScoped<IRepository<BookType>, Repository<BookType>>();
+            services.AddScoped<IRepository<Author>, Repository<Author>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute("CustomRoute", "{controller=home}/{action=Index}/{id?}");
+                });
+            });
+        }
+    }
+}

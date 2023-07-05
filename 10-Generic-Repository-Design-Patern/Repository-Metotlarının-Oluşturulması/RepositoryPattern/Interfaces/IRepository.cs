50779140f0f97af65436bca7a920354ca3e1a3da
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-07-06 00:39:39 +0300

    Generic Repository Design Pattern

---
+++ 10-Generic-Repository-Design-Patern/Repository-Metotlarının-Oluşturulması/RepositoryPattern/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Repository_Metotlarının_Oluşturulması.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Repository_Metotlarının_Oluşturulması.RepositoryPattern.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        List<T> GetAll();
+        List<T> GetActiveData();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void SpecialDelete(int id);
+        List<T> GetByFilter(Expression<Func<T, bool>> exp);
+        int Count();
+        bool Any(Expression<Func<T, bool>> exp);
+        List<T> SelectActivesByLimit(int limit);
+    }
+}

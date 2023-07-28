2a75cf0e3063b53f742837e2ff9e4a8953339bfa
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-07-28 15:29:31 +0300

    Authentication - Authorization

---
+++ 12-Authentication-Authorization/Authorization-Yetkilendirme/Areas/Management/Controllers/BookTypeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Authorization_Yetkilendirme.Context;
+using Authorization_Yetkilendirme.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Authorization_Yetkilendirme.RepositoryPattern.Interfaces;
+using Authorization_Yetkilendirme.Models;
+using Authorization_Yetkilendirme.Dto;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Authorization_Yetkilendirme.Areas.Management.Controllers
+{
+    [Area("Management")]
+    [Authorize(Policy = "AdminPolicy")]
+    public class BookTypeController : Controller
+    {
+        IBookTypeRepository _repositoryBookType;
+        public BookTypeController(IBookTypeRepository repositoryBookType)
+        {
+            _repositoryBookType = repositoryBookType;
+        }
+        public IActionResult BookTypeList()
+        {
+            List<BookTypeDto> bookTypes = _repositoryBookType.SelectBookTypeDto();
+
+            return View(bookTypes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookType bookType)
+        {
+            _repositoryBookType.Add(bookType);
+
+            return RedirectToAction("BookTypeList", "BookType", new { area = "Management" });
+        }
+
+        public IActionResult Edit(int id)
+        {
+            BookType bookType = _repositoryBookType.GetById(id);
+
+            return View(bookType);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BookType bookType)
+        {
+            _repositoryBookType.Update(bookType);
+
+            return RedirectToAction("BookTypeList", "BookType", new { area = "Management" });
+        }
+        // Hard Delete
+        public IActionResult HardDelete(int id)
+        {
+            _repositoryBookType.SpecialDelete(id);
+
+            return RedirectToAction("BookTypeList", "BookType", new { area = "Management" });
+        }
+    }
+}
---
+++ 12-Authentication-Authorization/Authorization-Yetkilendirme/Areas/Management/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization_Yetkilendirme.Areas.Management.Controllers
+{
+    [Area("Management")]
+    [Authorize(Policy = "AdminPolicy")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

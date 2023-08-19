9a32d6e7aebe4ad72ca7d5d49af23e1e12d749be
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-08-19 19:06:48 +0300

    Dotnet Core MVC Studies

---
+++ MVC Studies - 2/01-Giriş/FirstDotnetCoreImplementation/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDotnetCoreImplementation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "HomeController => Uygulama çalışıyor...";
+        }
+
+        public string Test()
+        {
+            return "Bu bir test action'udur.";
+        }
+        public IActionResult Greetings()
+        {
+            //ViewResult view_result = View();
+            //return view_result;
+
+            // View() metodu Controller sınıfı içerisinde tanımlıdır.
+            // View() metodu default olarak View katmanına gider ve Greetings view'ını arar.
+            // Rota ->> Views/Home/Greetings.cshtml
+            return View();
+        }
+        public IActionResult SecondGreetings()
+        {
+            return View();
+        }
+
+        public IActionResult RandomName() 
+        {
+            return View("TestView");
+        }
+    }
+}

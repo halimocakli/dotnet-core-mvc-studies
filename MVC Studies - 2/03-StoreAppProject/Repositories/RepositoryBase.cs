6dec93163fd0cadc159145bb9ba5d83c3d23ad1e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2023-08-29 13:37:58 +0300

    IProductRepository, IRepositoryBase, ProductRepository, RepositoryBase created.

---
+++ MVC Studies - 2/03-StoreAppProject/Repositories/RepositoryBase.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    // T şeklinde belirttiğimiz generic tipin, referans tipli (class) ya da new'lenebilir olmasını istiyoruz.
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class, new()
+    {
+        // DEPENDENCY INJECTION
+        protected readonly RepositoryContext _context;
+
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return trackChanges
+                ? _context.Set<T>()
+                : _context.Set<T>().AsNoTracking();
+        }
+    }
+}
